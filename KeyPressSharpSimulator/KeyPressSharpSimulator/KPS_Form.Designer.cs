0dee980cea3f62f441c3a13f67382c5491bec684
Sharjeel Qasim <Sharjeel.Qasim@mavenx.gg>
2023-08-09 12:56:30 +0500

    KPS init changes

---
+++ KeyPressSharpSimulator/KeyPressSharpSimulator/KPS_Form.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace KeyPressSharpSimulator
+{
+    partial class KPS_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(KPS_Form));
+            this.Start_Button = new System.Windows.Forms.Button();
+            this.Stop_Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start_Button
+            // 
+            this.Start_Button.Location = new System.Drawing.Point(34, 24);
+            this.Start_Button.Name = "Start_Button";
+            this.Start_Button.Size = new System.Drawing.Size(126, 55);
+            this.Start_Button.TabIndex = 0;
+            this.Start_Button.Text = "Start";
+            this.Start_Button.UseVisualStyleBackColor = true;
+            this.Start_Button.Click += new System.EventHandler(this.Start_Button_Click);
+            // 
+            // Stop_Button
+            // 
+            this.Stop_Button.Location = new System.Drawing.Point(167, 24);
+            this.Stop_Button.Name = "Stop_Button";
+            this.Stop_Button.Size = new System.Drawing.Size(126, 55);
+            this.Stop_Button.TabIndex = 1;
+            this.Stop_Button.Text = "Stop";
+            this.Stop_Button.UseVisualStyleBackColor = true;
+            this.Stop_Button.Click += new System.EventHandler(this.Stop_Button_Click);
+            // 
+            // KPS_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 101);
+            this.Controls.Add(this.Stop_Button);
+            this.Controls.Add(this.Start_Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "KPS_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KPS";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button Start_Button;
+        private System.Windows.Forms.Button Stop_Button;
+    }
+}
+
---
+++ KeyPressSharpSimulator/KeyPressSharpSimulator/KPS_Form.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KeyPressSharpSimulator
+{
+    public partial class KPS_Form : Form
+    {
+        private Timer KPS_Timer = new Timer();
+        public KPS_Form()
+        {
+            InitializeComponent();
+
+            KPS_Timer.Interval = 3000;
+            KPS_Timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            SendKeys.SendWait("{DOWN}");
+        }
+
+        private void Start_Button_Click(object sender, EventArgs e)
+        {
+            KPS_Timer.Start();
+        }
+
+        private void Stop_Button_Click(object sender, EventArgs e)
+        {
+            KPS_Timer.Stop();
+        }
+
+        private void Hide_Button_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
